bce116a9de982be79f60b728b48f56f5ff7f5796
Hugo Hörnquist <hugo.hornquist@gmail.com>
2017-09-21 14:20:53 +0200

    code much better

--- main.c
+++ main.c
@@ -4,20 +4,13 @@
 // includes sleep
 #include <unistd.h>
 
-//include <linux/fb.h>
-//include <fcntl.h>
-
-#define LINES 66
-#define HEIGHT 800
-#define WIDTH 1280
-#define FONT_HEIGHT 12
-
 #define byte unsigned char
 
 #define B 0
 #define G 1
 #define R 2
-#define C G
+
+#define seconds * 1e6
 
 /*
  * Programs which display stuff on the pixels left over in a tty
@@ -29,40 +22,53 @@
  * Possibly some stick figures and a clock!
  */
 
+// hsv -> rgb
+// Hue Saturation Value
+
+void setColor(
+		byte* pixel,
+		unsigned int x,
+		unsigned int y,
+		unsigned int loop)
+{
+	if (x % 0x200 > 0xFF && x % 0x200 < 0x200) {
+		pixel[R] = -x;
+		pixel[G] = -x;
+	} else {
+		pixel[R] = x;
+		pixel[G] = x;
+	}
+}
+
 int main() {
 	srandom(1);
+	unsigned static int LINES = 66;
+	unsigned static int HEIGHT = 800;
+	unsigned static int WIDTH = 1280;
+	unsigned static int FONT_HEIGHT = 12;
 
-	int usable_lines = HEIGHT - FONT_HEIGHT * LINES;
-	int data_size = usable_lines * WIDTH * 4;
+	unsigned int usable_lines = HEIGHT - FONT_HEIGHT * LINES;
+	unsigned int data_size = usable_lines * WIDTH * 4;
 	byte* data = malloc(data_size * sizeof(byte));
 
 	FILE* f = fopen("/dev/fb0", "wb");
-	for (int loop = 0;; loop++) {
-		for (int i = 0; i < data_size; i += 4) {
-			switch (loop % 3) {
-				case 0: // BLUE
-					data[i] = 0xFF;
-					data[i+1] = 0;
-					data[i+2] = 0;
-					break;
-				case 1: // Yellow
-					data[i] = 0;
-					data[i+1] = 0xFF;
-					data[i+2] = 0xFF;
-					break;
-				case 2:
-					data[i]   = 0xFF;
-					data[i+1] = 0;
-					data[i+2] = 0xFF;
+
+	printf("WIDTH: %i\tusable_lines: %i\tdata_size: %i\n", WIDTH, usable_lines, data_size);
+
+	for (unsigned int loop = 0; ; loop++) {
+		for (unsigned int x = 0; x < WIDTH; x++) {
+			for (unsigned int y = 0; y < usable_lines; y += 1) {
+				unsigned int i = (y * WIDTH + x) * 4;
+				setColor(data + i, x, y, loop);
 			}
 		}
+
 
 		fseek(f, (HEIGHT * WIDTH * 4) - data_size, SEEK_SET);
 		/* int read = */
 		fwrite(data, sizeof(byte), data_size, f);
 		//printf("%i, %i\n", data_size, read);
-		int sleep_time = random() % 5;
-		sleep(sleep_time);
+		usleep (0.1 seconds);
 	}
 	fclose(f);
 }
